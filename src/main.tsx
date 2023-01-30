d3fd5111d215cff6e0d757cb8e7860c6caa1fc5f
Vbeo147 <vbeo147@gmail.com>
2023-01-30 11:35:37 +0900

    firebase and redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { rootReducer } from "./rootReducer";
+import ReduxThunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
