ad544dbb800a9e94034bca3136e9600925cde83f
Vbeo147 <vbeo147@gmail.com>
2023-02-01 15:16:03 +0900

    modify vite server port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 1234,
+  },
   plugins: [react()],
   base: "/react-blog_v2/",
 });
