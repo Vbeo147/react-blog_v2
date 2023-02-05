c55617f913ea3a25bd96e0c0d4d11bdc3133c2af
Vbeo147 <vbeo147@gmail.com>
2023-02-05 14:13:19 +0900

    Write Form

---
+++ src/interfaces/FormTypes.ts
@@ -0,0 +1,5 @@
+export interface IForm {
+  title: string;
+  content: string;
+  tag: string;
+}
--- src/routes/Write.tsx
+++ src/routes/Write.tsx
@@ -1,7 +1,41 @@
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { IForm } from "../interfaces/FormTypes";
 
 function Write() {
-  return <div>Write</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IForm>();
+  const onSubmit: SubmitHandler<IForm> = (data) => {
+    console.log(data);
+    reset({ title: "", content: "" });
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input
+          autoComplete="off"
+          placeholder="title"
+          {...register("title", { required: true })}
+        />
+        <input
+          autoComplete="off"
+          placeholder="content"
+          {...register("content")}
+        />
+      </div>
+      <div>
+        <select {...register("tag", { required: true })}>
+          <option>test1</option>
+          <option>test2</option>
+          <option>test3</option>
+        </select>
+      </div>
+      <button type="submit">Enter</button>
+    </form>
+  );
 }
 
 export default Write;
