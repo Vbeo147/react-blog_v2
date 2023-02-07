5a65182afcb68cfab1132c75671a7b4502c4dda3
Vbeo147 <vbeo147@gmail.com>
2023-02-07 12:08:41 +0900

    modify img condition

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,8 +18,14 @@ function Menu() {
   return (
     <div className="menu-flex">
       <div className="menu-login-flex">
-        <span>{github?.login}</span>
-        <img className="menu-img" src={github?.avatar_url} alt="" />
+        <span style={{ marginRight: "12px" }}>{github?.login}</span>
+        {github?.avatar_url && auth?.photoURL ? (
+          <img
+            className="menu-img"
+            src={github.avatar_url || auth.photoURL}
+            alt=""
+          />
+        ) : null}
         <button onClick={onLogClick} style={{ cursor: "pointer" }}>
           {auth ? "Log Out" : "Log In"}
         </button>
